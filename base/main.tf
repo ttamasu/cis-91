83b7a477ba7bbe2a813fe293076f40a2ab4905c7
Mike Matera <maximus@fatboycentral.com>
2021-08-16 16:50:05 -0700

    Remove 80 from port list.

--- base/main.tf
+++ base/main.tf
@@ -57,7 +57,7 @@ resource "google_compute_firewall" "ssh-rule" {
   network = google_compute_network.vpc_network.name
   allow {
     protocol = "tcp"
-    ports = ["22", "80"]
+    ports = ["22"]
   }
   source_ranges = ["0.0.0.0/0"]
 }
